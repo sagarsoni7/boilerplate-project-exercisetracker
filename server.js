f571adc6fde9599f6f958087b6ac1fef096a0a6b
Sagar Soni <sagar.soni@zoho.com>
2019-08-20 02:05:42 +0530

    handling POST req

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const urlencodedParser=bodyParser.urlencoded({extended: false});
 
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect("mongodb+srv://sagarsoni:nopassword123@cluster0-onc9h.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true});
+const excerciseSchema=mongoose.Schema({
+  username:String,
+  exercises:[{
+    description:String,
+    duration:Number,
+    date:Date
+  }]
+});
+const exerciseModel=new mongoose.model("Exercise",excerciseSchema);
 
 app.use(cors())
 
@@ -18,7 +28,41 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/exercise/new-user",urlencodedParser,(req,res)=>{
+  let addUser=new exerciseModel({username: req.body.username})
+  addUser.save((err,data)=>{
+    if(err) throw err;
+    res.json({
+      username:data.username,
+      _id:data._id
+    })
+  });
+  
+})
 
+app.post("/api/exercise/add",urlencodedParser,(req,res)=>{
+  let objectToPush={
+    description:req.body.description,
+    duration:req.body.duration,
+    date:req.body.date
+  }
+  exerciseModel.findById(req.body.userId,(err,data)=>{
+    // console.log(data);
+    data.exercises.push(objectToPush);
+    data.save((err,data)=>{
+      if(err) throw err;
+      res.json({
+        username:data.username,
+        description:objectToPush.description,
+        duration: objectToPush.duration,
+        id: data.id,
+        date:objectToPush.date
+      });
+    });
+    
+  })
+  // res.send("Hor byi")
+})
 // Not found middleware
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
@@ -42,6 +86,9 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
